fc2ae2bf8b8baa0d80a4adcd06e587f3ad066955
ggwpez <oliver.tale@web.de>
2015-08-15 12:46:09 +0200

    Update idtList.asm

--- idtList.asm
+++ idtList.asm
@@ -1,51 +1,47 @@
 [BITS 32]
 extern _ir_event_handler
 
+; ISR
 %assign ISR_C 32
 
-; Template for a single interrupt routine:
 %macro ISRs 1
     [global _isr%1]
     _isr%1:
 	%if (%1!=8) && (%1<10 || %1>14)
-	    push dword 0
+	    push 0
 	%endif
-	push dword %1
+	push %1
 	jmp ir_common_stub
 %endmacro
 
-; Create the interrupt-routines
 %assign routine_nr_s 0
 %rep ISR_C
     ISRs routine_nr_s
     %assign routine_nr_s routine_nr_s+1
 %endrep
 
+; IRQ
 %assign IRQ_C 16
 
 %macro IRQs 1
     [global _irq%1]
     _irq%1:
-	push dword 0
-	push dword %1+32
+	push 0
+	push %1+32
 	jmp ir_common_stub
 %endmacro
 
-; Create the interrupt-routines
 %assign routine_nr_q 0
 %rep IRQ_C
     IRQs routine_nr_q
     %assign routine_nr_q routine_nr_q+1
 %endrep
 
-extern _irq_event_handler
-[global _irq_tail]
-
+; General Interrupt method
 ir_common_stub:
- ; CPU-Zustand sichern
-    push eax
-    push ebx
-    push ecx
+    push eax	;Pushad is for plebs, push them manually
+    push ebx	;Things like EFLAGS and so on, are automatically
+    push ecx	;saved by the CPU
     push edx
     push ebp
     push esi
@@ -66,7 +62,7 @@ ir_common_stub:
     call _ir_event_handler
 
 _ir_tail:
-    mov esp, eax
+    mov esp, eax	;get the new cpu_state_t* as return value from the C method
 
     pop gs
     pop fs
@@ -81,9 +77,7 @@ _ir_tail:
     pop ebx
     pop eax
     add esp, 8
-
-;    // Ruecksprung zum unterbrochenen Code
-    iret
+    iret		;iret also makes an sti
 
 [global _syscall]
 _syscall:
@@ -94,10 +88,10 @@ _syscall:
 	mov esi, [esp+20]
 	mov edi, [esp+24]
 
-	int 37
+	int 37		;interrupt for syscalls, maybe change the number
 	ret
 
 [global _nop]
-_nop:
+_nop:			;nop
     nop
     ret
