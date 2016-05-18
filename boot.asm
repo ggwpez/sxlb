e0def82bfb82422eeeaa2897e305a19408707f49
ggwpez <oliver.tale@web.de>
2016-05-18 22:16:34 +0200

    Paging still not annoying me...

---
+++ boot.asm
@@ -0,0 +1,24 @@
+%include "multiboot2.asm"
+
+[global _start]
+[extern _main]
+[global _kernel_start]
+[global _kernel_end]
+
+[section .text]
+
+_start:
+    mov esp, 0x200000
+
+    push eax
+    push ebx
+
+    call _main
+
+    add esp, 8
+
+    cli ; falls der Kernel bis hier her kommt, CPU anhalten
+    hlt
+
+_kernel_start:
+_kernel_end:
